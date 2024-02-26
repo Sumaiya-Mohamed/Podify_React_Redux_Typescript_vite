84465b8f3203b9b85abed3641eaabd94dffe35f9
Sumaiya Mohamed <sumimaya13@gmail.com>
2024-02-26 11:03:21 +0200

    Delete src directory

--- src/Client.tsx
+++
@@ -1,5 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://dqmxvwjzbfflcalqdumj.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRxbXh2d2p6YmZmbGNhbHFkdW1qIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDcyNDIyMzYsImV4cCI6MjAyMjgxODIzNn0.wSmYzMMiEGck3rqHLwsF7Q67TYLHHudNLHwWPTMGYAk'
-export const supabase = createClient(supabaseUrl, supabaseKey)
--- src/data.tsx
+++
@@ -1,4 +0,0 @@
-  
-  
- export const allGenres = [  "True Crime and Investigative Journalism",  "Comedy",  "News",  "Business",  "Technology",  "Education",  "History",  "Health",  "Science",  "Politics",  "Sports",  "Entertainment",  "Music",  "Food",  "Travel",  "Storytelling",  "Interviews",  "Fiction",  "Self-Improvement",  "Spirituality",  "Documentary",  "Parenting",  "Gaming",  "Art",  "Society & Culture",  "Hobbies",  "Fitness",  "Fashion",  "Personal  Growth",  "Philosophy",  "Relationships",  "Languages",  "Technology",  "Books",  "Psychology",  "True Stories",  "Horror",  "Design",  "Film",  "Environment",  "Marketing",  "Motivation",  "Investing",  "Astrology",  "Career",  "Home Improvement",  "Mental Health",  "Nature",  "Photography",  "Poetry",  "Science Fiction",  "Sustainability",  "Theater",  "Travel",  "Videogames",  "Wellness",  "Writing", "Featured"]
-
